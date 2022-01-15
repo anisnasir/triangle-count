4641462db7cce3a3caf51b52c4deee08b1e504e0
Anis Nasir <dradnan89@hotmail.com>
2022-01-15 18:10:54 +0100

    First  commit

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -42,9 +42,7 @@ public class Main {
 
         }
 
-        int maxDegree = getMaxDegree(nodeMap);
-
-        System.out.println("max degree: " + maxDegree);
+        
     }
 
     private static int getMaxDegree(NodeMap nodeMap) {
